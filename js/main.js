3bfad9260a7c5f026791d330bf654e3da601db8e
bogmw <401714149git>
2016-03-09 16:41:26 +0200

    fixed adding do as object

--- js/main.js
+++ js/main.js
@@ -8,25 +8,39 @@ function CheckBrowser() {
 
 function generateItem(item){
     var newLi = document.createElement('li');
-    newLi.innerHTML = JSON.parse(localStorage[item]).text;
+    newLi.innerHTML = JSON.parse(localStorage.getItem(item)).text;
     document.getElementById('list').appendChild(newLi)
 }
 
 function doShowAll() {
     if (CheckBrowser()) {
-        for (var i = 0; i < localStorage.length; i++) {
-            generateItem(i);
+        if (checkToDo() > 0) {
+            for (var i = 0; i < checkToDo(); i++) {
+                var gKey = 'toDo' + i;
+                generateItem(gKey);
+            }
         }
     } else {
         alert('Your browser do not support local storage');
     }
 }
 
+function checkToDo (){
+    var toDoCount = 0;
+   for (var key in localStorage) {
+       if (key.indexOf("toDo") !== -1) {
+           toDoCount++;
+       };
+   };
+    return toDoCount;
+}
+
 function doAdd () {
     var newDoText = document.getElementById('taskInput').value;
     var newDoStatus = 'false';
     var newDo = {text : newDoText, status: newDoStatus};
-    var index = localStorage.length;
-    localStorage[index] = JSON.stringify(newDo);
+    var doCount = checkToDo();
+    var index = 'toDo'+ doCount;
+    localStorage.setItem(index, JSON.stringify(newDo));
     generateItem(index);
 }
